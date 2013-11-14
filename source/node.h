16555984e5fff75c04f917669025a794233e3adc
Melnik96 <dimamelnichuk11@gmail.com>
2013-11-14 17:29:38 +0000

    a ibu

---
+++ source/node.h
@@ -0,0 +1,38 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2013  <copyright holder> <email>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef NODE_H
+#define NODE_H
+
+struct socket {
+  char name[32];
+  struct node* node;
+  void* value;
+};
+
+struct node {
+  char type[64];
+  
+  struct socket* inputs;
+  struct socket* outputs;
+  
+  void(*process)(void*);
+};
+
+#endif // NODE_H
